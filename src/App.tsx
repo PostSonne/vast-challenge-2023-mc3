f488b204b0a67a67664f2d785910cd043216fb23
ananichevl <ananichev@phystech.edu>
2024-06-19 14:07:49 +0200

    #remove files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 
-function App() {
+const App: React.FC = () => {
+  useEffect(() => {
+    const svg = d3.select('#d3-container')
+        .append('svg')
+        .attr('width', 600)
+        .attr('height', 400);
+
+    svg.append('circle')
+        .attr('cx', 300)
+        .attr('cy', 200)
+        .attr('r', 100)
+        .attr('fill', 'blue');
+  }, []);
+
+  const chart = ForceGraph(miserables, {
+      nodeId: d => d.id,
+      nodeGroup: d => d.group,
+      nodeTitle: d => `${d.id}\n${d.group}`,
+      linkStrokeWidth: l => Math.sqrt(l.value),
+      width,
+      height: 600,
+      invalidation // a promise to stop the simulation when the cell is re-run
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div id="d3-container"></div>
+      </div>
   );
-}
+};
 
 export default App;
